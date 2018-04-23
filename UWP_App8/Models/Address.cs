876fa498138ad939e700123f77693f39fc8cd933
Chris Darnell <csdarnell@gmail.com>
2018-04-23 14:37:43 -0400

    data binding

--- UWP_App8/Models/Address.cs
+++ UWP_App8/Models/Address.cs
@@ -1,16 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UWP_App8.Models
 {
-    public class Address
+    public class Address : INotifyPropertyChanged
     {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
+        private string _Street;
+        public string Street
+        {
+            get { return _Street; }
+            set { if (_Street != value)
+                {
+                    _Street = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Street"));
+                }
+            }
+        }
+        private string _City;
+        public string City
+        {
+            get { return _City; }
+            set
+            {
+                if (_City !=value)
+                {
+                    _City = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("City"));
+                }
+                    
+            }
+
+        }
+        private string _State;
+        public string State
+        {
+            get { return _State; }
+            set
+            {
+                if (_State != value)
+                {
+                    _State = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("State"));
+                }
+            }
+        }
+        private string _Zip;
+        public string Zip
+        { 
+            get { return _Zip; }
+            set
+            {
+                if (_Zip != value)
+                {
+                    _Zip = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Zip"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
